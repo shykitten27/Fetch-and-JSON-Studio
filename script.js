27f9a62e37c001fad6fc7ad73947f57536780a5b
D Seidel <67442972+shykitten27@users.noreply.github.com>
2021-06-21 17:13:54 -0500

    dev

--- script.js
+++ script.js
@@ -2,19 +2,33 @@ window.addEventListener("load", function() {
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
         // Access the JSON in the response
         response.json().then( function(json) {
-            //console.log(response);
+            let jsonLength = json.length;
+            let astronauntHours = json[i].hours; 
+            let astronauntActive = json[i].active; 
+            let astronauntSkills = json[i].skills; 
+            for(i=0; i<jsonLength-1; i++){
+                if(json[i].active === true) {   
+                   json[i].active.style.color = "green";
+                   }
+            }
+            /* save json[i].property as variable then pass that 
+            into div.innerHTML string.
+            Test conditional inside of astronaunt variable
+            to set the attribute type, i.e style.color = "green" */
+
             const div = document.getElementById("container");
             // Add HTML that includes the JSON data
             div.innerHTML = `
-            <div class="astronaut">
+            <div class="astronaut">  
                 <div class="bio">
-                    <h3>${json[0].firstName} ${json[0].lastName}</h3>
+                    <h3>${json[i].firstName} ${json[i].lastName}</h3>
                     <ul>
-                        <li>Hours in space: ${json[0].hoursInSpace}</li>
-                        <li>Active: ${json[0].active}</li>
-                        <li>Skills: ${json[0].skills}</li>
+                        <li>Hours in space: ${json[i].hoursInSpace}</li>
+                        <li>Active: ${json[i].active}</li>
+                        <li>Skills: ${json[i].skills}</li>
                     </ul>
                 </div>
+                <img class="avatar" src="${json[i].picture}">
             </div>
             `;            
         });
