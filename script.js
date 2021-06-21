f6d175fef0e391d222c4b7ffbda90cd7de2c8218
D Seidel <67442972+shykitten27@users.noreply.github.com>
2021-06-21 17:17:14 -0500

    dev

--- script.js
+++ script.js
@@ -2,33 +2,36 @@ window.addEventListener("load", function() {
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
         // Access the JSON in the response
         response.json().then( function(json) {
-            let jsonLength = json.length;
-            let astronauntHours = json[i].hours; 
-            let astronauntActive = json[i].active; 
-            let astronauntSkills = json[i].skills; 
-            for(i=0; i<jsonLength-1; i++){
-                if(json[i].active === true) {   
-                   json[i].active.style.color = "green";
-                   }
-            }
             /* save json[i].property as variable then pass that 
             into div.innerHTML string.
             Test conditional inside of astronaunt variable
             to set the attribute type, i.e style.color = "green" */
 
+            let jsonLength = json.length;
+            let astronauntFirstName = json[i].firstName;
+            let astronauntLastName = json[i].lastName;
+            let astronauntHours = json[i].hours; 
+            let astronauntActive = json[i].active; 
+            let astronauntSkills = json[i].skills; 
+            let astronauntPicture = json[i].picture;
+            for(i=0; i<jsonLength-1; i++){
+                if(json[i].active === true) {   
+                   json[i].active.style.color = "green";
+                   }
+            }
             const div = document.getElementById("container");
             // Add HTML that includes the JSON data
             div.innerHTML = `
             <div class="astronaut">  
                 <div class="bio">
-                    <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                    <h3>${astronauntFirstName} ${astronauntLastName}</h3>
                     <ul>
-                        <li>Hours in space: ${json[i].hoursInSpace}</li>
-                        <li>Active: ${json[i].active}</li>
-                        <li>Skills: ${json[i].skills}</li>
+                        <li>Hours in space: ${astronauntHours}</li>
+                        <li>Active: ${astronauntActive}</li>
+                        <li>Skills: ${astronauntSkills}</li>
                     </ul>
                 </div>
-                <img class="avatar" src="${json[i].picture}">
+                <img class="avatar" src="${astronauntPicture}">
             </div>
             `;            
         });
