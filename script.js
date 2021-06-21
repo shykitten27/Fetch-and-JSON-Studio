16c85ee4e3ac00d1fa4627062a9c7c4d07edf3a4
D Seidel <67442972+shykitten27@users.noreply.github.com>
2021-06-20 22:18:02 -0500

    dev

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// TODO: add code here
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        // Access the JSON in the response
+        response.json().then( function(json) {
+            //console.log(response);
+            const div = document.getElementById("container");
+            // Add HTML that includes the JSON data
+            div.innerHTML = `
+            <div class="astronaut">
+                <div class="bio">
+                    <h3>${json[0].firstName} ${json[0].lastName}</h3>
+                    <ul>
+                        <li>Hours in space: ${json[0].hoursInSpace}</li>
+                        <li>Active: ${json[0].active}</li>
+                        <li>Skills: ${json[0].skills}</li>
+                        <li>Skills: ${json[0].picture}</li>
+                    </ul>
+                </div>
+
+            </div>
+            `;            
+        });
+    }); 
+});
